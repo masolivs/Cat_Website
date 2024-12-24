38fcd243f4a3a3750c91b3e5975834749aa1ece3
Mariana S Oliveira <marianasoli621@gmail.com>
2024-12-24 11:27:49 -0300

    separação das pastas e inicio do projeto

---
+++ public/script.js
@@ -0,0 +1,14 @@
+document.getElementById('generateCat').addEventListener('click', async () => {
+    const apiUrl = 'https://api.thecatapi.com/v1/images/search';
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      const catImage = document.getElementById('catImage');
+      catImage.src = data[0].url; 
+      catImage.hidden = false; 
+    } catch (error) {
+      console.error('Erro ao carregar imagem:', error);
+      alert('Não foi possível gerar o gatinho. Tente novamente mais tarde.');
+    }
+  });
+  
